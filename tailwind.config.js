f0c57265ec65d2996ac20450438c9bdcc86cde8c
yohan <yovirinc@gmail.com>
2025-12-10 19:42:23 +0100

    working.

---
+++ tailwind.config.js
@@ -0,0 +1,6 @@
+/** @type {import('tailwindcss').Config} */
+export const content = ["./src/**/*.{html, js, tsx, jsx, ts, php}"];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
